e9000e45cecf0185a388a7ca5e0f7c935673f1a4
iqonese <86726977+iqonese@users.noreply.github.com>
2024-05-16 01:21:33 +0500

    Add files via upload

---
+++ loops.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+//MARK: - Homework #15
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else {return}
+    
+    URLSession.shared.dataTask(with: url) {data, response, error in
+        if error != nil {
+            print("Error: \((error?.localizedDescription)!)")
+            return
+            
+        }
+        if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print(response)
+            print("\n")
+            guard let data = data else {return}
+            let dataAsString = String(data: data, encoding: .utf8)
+            
+            // result Data
+            guard let resultData = dataAsString else {return}
+            print(resultData)
+        } else {
+            print("Status code: \((response as? HTTPURLResponse)!.statusCode)")
+        }
+        
+    }.resume()
+}
+
+let baseURL: String = "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+
+
+
+var sample: () = getData(urlRequest: baseURL)
+
